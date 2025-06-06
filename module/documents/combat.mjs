2cf68b7f177c59d83b7da96ee5f41bb397c8e2ea
WBHarry <williambjrklund@gmail.com>
2025-06-06 21:34:07 +0200

    Some cleneaup

--- module/documents/combat.mjs
+++ module/documents/combat.mjs
@@ -20,34 +20,4 @@ export default class DhpCombat extends Combat {
 
         return a.name.localeCompare(b.name);
     }
-
-    // async useActionToken(combatantId) {
-    //     const automateActionPoints = await game.settings.get(
-    //         SYSTEM.id,
-    //         SYSTEM.SETTINGS.gameSettings.Automation.ActionPoints
-    //     );
-
-    //     if (game.user.isGM) {
-    //         if (this.system.actions < 1) return;
-
-    //         const update = automateActionPoints
-    //             ? { 'system.activeCombatant': combatantId, 'system.actions': Math.max(this.system.actions - 1, 0) }
-    //             : { 'system.activeCombatant': combatantId };
-
-    //         await this.update(update);
-    //     } else {
-    //         const update = automateActionPoints
-    //             ? { 'system.activeCombatant': combatantId, 'system.actions': this.system.actions + 1 }
-    //             : { 'system.activeCombatant': combatantId };
-
-    //         await game.socket.emit(`system.${SYSTEM.id}`, {
-    //             action: socketEvent.GMUpdate,
-    //             data: {
-    //                 action: GMUpdateEvent.UpdateDocument,
-    //                 uuid: this.uuid,
-    //                 update: update
-    //             }
-    //         });
-    //     }
-    // }
 }
