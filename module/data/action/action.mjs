9f70ebd561d808b4685e55858ff0c34e0c7a7c90
Dapoolp <elcatnet@gmail.com>
2025-06-13 11:56:12 +0200

    resources

--- module/data/action/action.mjs
+++ module/data/action/action.mjs
@@ -308,7 +308,7 @@ export class DHResourceAction extends DHBaseAction {
             ...extraDefineSchema('target'),
             ...extraDefineSchema('effects'),
             resource: new fields.SchemaField({
-                type: new fields.StringField({ choices: [], initial: "", label: "Resource" }),
+                type: new fields.StringField({ choices: [], blank: true, required: false, initial: "", label: "Resource" }),
                 value: new fields.NumberField({ initial: 0, label: "Value" })
             })
         }
