5463f7152684d3ed0f244f275da87410b0453e6f
Joaquin Pereyra <joaquinpereyra98@users.noreply.github.com>
2025-06-17 18:27:33 -0300

    FIX: Remove the psudo-documents because they will be used.

--- module/data/_module.mjs
+++ module/data/_module.mjs
@@ -8,4 +8,3 @@ export * as items from './item/_module.mjs';
 export { actionsTypes } from './action/_module.mjs';
 export * as messages from './chat-message/_modules.mjs';
 export * as fields from './fields/_module.mjs';
-export * as pseudoDocuments from './pseudo-documents/_module.mjs';
--- module/data/fields/_module.mjs
+++ module/data/fields/_module.mjs
@@ -1,3 +1,2 @@
 export { default as FormulaField } from './formulaField.mjs';
 export { default as ForeignDocumentUUIDField } from './foreignDocumentUUIDField.mjs';
-export { default as PseudoDocumentsField } from './pseudoDocumentsField.mjs';
