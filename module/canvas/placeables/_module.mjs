9c30e51546739114afd1664be4100e93fdcc356d
WBHarry <89362246+WBHarry@users.noreply.github.com>
2025-07-04 22:25:15 +0200

    Update module/canvas/placeables/_module.mjs
    Co-authored-by: joaquinpereyra98 <24190917+joaquinpereyra98@users.noreply.github.com>

--- module/canvas/placeables/_module.mjs
+++ module/canvas/placeables/_module.mjs
@@ -1,5 +1,3 @@
-import { default as DhMeasuredTemplate } from './measuredTemplate.mjs';
-import { default as DhRuler } from './ruler.mjs';
-import { default as DhTokenRuler } from './tokenRuler.mjs';
-
-export { DhMeasuredTemplate, DhRuler, DhTokenRuler };
+export { default as DhMeasuredTemplate } from './measuredTemplate.mjs';
+export { default as DhRuler } from './ruler.mjs';
+export { default as DhTokenRuler } from './tokenRuler.mjs';
