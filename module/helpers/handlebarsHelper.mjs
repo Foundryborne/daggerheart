bdc9671d69a837b3e73a9d12bd0de1b289ffbf25
WBHarry <williambjrklund@gmail.com>
2025-07-25 02:31:54 +0200

    .

--- module/helpers/handlebarsHelper.mjs
+++ module/helpers/handlebarsHelper.mjs
@@ -47,7 +47,7 @@ export default class RegisterHandlebarsHelpers {
 
     static rollParsed(value, actor, item, numerical) {
         const isNumerical = typeof numerical === 'boolean' ? numerical : false;
-        const result = itemAbleRollParse(value, actor, item);
-        return isNumerical && !result ? 0 : result;
+        const result = itemAbleRollParse(value, actor.getRollData(), item);
+        return isNumerical ? (!result ? 0 : Number(result)) : result;
     }
 }
