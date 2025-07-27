998e91004e02a297c637aa6bc4761bfddd2fe324
psitacus <walther.johnson@ucalgary.ca>
2025-07-27 14:38:07 -0600

    fix weird ternary

--- module/applications/dialogs/d20RollDialog.mjs
+++ module/applications/dialogs/d20RollDialog.mjs
@@ -91,10 +91,11 @@ export default class D20RollDialog extends HandlebarsApplicationMixin(Applicatio
             context.roll = this.roll;
             context.rollType = this.roll?.constructor.name;
             context.rallyDie = this.roll.rallyChoices;
-            context.experiences = this.config.data?.experiences ? Object.keys(this.config.data.experiences).map(id => ({
+            const experiences = this.config.data?.experiences || {};
+            context.experiences = Object.keys(experiences).map(id => ({
                 id,
-                ...this.config.data.experiences[id]
-            })) : [];
+                ...experiences[id]
+            }));
             context.selectedExperiences = this.config.experiences;
             context.advantage = this.config.roll?.advantage;
             context.disadvantage = this.config.roll?.disadvantage;
