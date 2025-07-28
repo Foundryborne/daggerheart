078ca3262d442a1b1afe633dc38c40fff53546f9
WBHarry <williambjrklund@gmail.com>
2025-07-28 22:50:08 +0200

    Added basic chat-message.hbs

--- module/documents/chatMessage.mjs
+++ module/documents/chatMessage.mjs
@@ -6,8 +6,10 @@ export default class DhpChatMessage extends foundry.documents.ChatMessage {
                 _source: this.system._source
             });
 
+        const actor = game.actors.get(this.speaker.actor);
+        const actorData = actor ?? { img: 'icons/svg/mystery-man.svg', name: '' };
         /* We can change to fully implementing the renderHTML function if needed, instead of augmenting it. */
-        const html = await super.renderHTML();
+        const html = await super.renderHTML({ actor: actorData });
         this.applyPermission(html);
 
         if (this.type === 'dualityRoll') {
