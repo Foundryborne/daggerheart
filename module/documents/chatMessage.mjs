74ccd793908d1cb060b4906dea76a805f4f96748
moliloo <dev.murilobrito@gmail.com>
2025-07-29 00:03:33 -0300

    remove console log

--- module/documents/chatMessage.mjs
+++ module/documents/chatMessage.mjs
@@ -13,7 +13,6 @@ export default class DhpChatMessage extends foundry.documents.ChatMessage {
         };
         /* We can change to fully implementing the renderHTML function if needed, instead of augmenting it. */
         const html = await super.renderHTML({ actor: actorData, author: this.author });
-        console.log(this.author);
         this.applyPermission(html);
 
         if (this.type === 'dualityRoll') {
