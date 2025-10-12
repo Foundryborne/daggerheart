b6a8722a2dbb4aeb69f71d0f84f045c0bbb6d54f
Carlos Fernandez <cfern1990@gmail.com>
2025-10-12 00:54:52 -0400

    Remove item description fieldset

--- module/applications/sheets/items/domainCard.mjs
+++ module/applications/sheets/items/domainCard.mjs
@@ -39,7 +39,6 @@ export default class DomainCardSheet extends DHBaseItemSheet {
         const context = await super._prepareContext(options);
         context.domain = CONFIG.DH.DOMAIN.allDomains()[this.document.system.domain];
         context.domainChoices = CONFIG.DH.DOMAIN.orderedDomains();
-
         return context;
     }
 }
