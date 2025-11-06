e8961afc5ed8afcf143db95e9d8632302e218a8e
WBHarry <williambjrklund@gmail.com>
2025-11-06 21:48:41 +0100

    Added the clown car

--- module/applications/hud/tokenHUD.mjs
+++ module/applications/hud/tokenHUD.mjs
@@ -1,8 +1,11 @@
+import { shuffleArray } from '../../helpers/utils.mjs';
+
 export default class DHTokenHUD extends foundry.applications.hud.TokenHUD {
     static DEFAULT_OPTIONS = {
         classes: ['daggerheart'],
         actions: {
-            combat: DHTokenHUD.#onToggleCombat
+            combat: DHTokenHUD.#onToggleCombat,
+            togglePartyTokens: DHTokenHUD.#togglePartyTokens
         }
     };
 
@@ -19,6 +22,10 @@ export default class DHTokenHUD extends foundry.applications.hud.TokenHUD {
     async _prepareContext(options) {
         const context = await super._prepareContext(options);
 
+        context.partyOnCanvas = this.actor.system.partyMembers.some(member => member.getActiveTokens().length > 0);
+        context.icons.toggleParty = 'systems/daggerheart/assets/icons/arrow-dunk.png';
+        context.actorType = this.actor.type;
+        context.usesEffects = this.actor.type !== 'party';
         context.canToggleCombat = DHTokenHUD.#nonCombatTypes.includes(this.actor.type)
             ? false
             : context.canToggleCombat;
@@ -56,6 +63,102 @@ export default class DHTokenHUD extends foundry.applications.hud.TokenHUD {
             else await TokenDocument.implementation.createCombatants(tokens);
         } catch (err) {
             ui.notifications.warn(err.message);
+        }
+    }
+
+    static async #togglePartyTokens(_, button) {
+        const icon = button.querySelector('img');
+        icon.classList.toggle('flipped');
+        button.dataset.tooltip = game.i18n.localize(
+            icon.classList.contains('flipped')
+                ? 'DAGGERHEART.APPLICATIONS.HUD.tokenHUD.retrievePartyTokens'
+                : 'DAGGERHEART.APPLICATIONS.HUD.tokenHUD.depositPartyTokens'
+        );
+
+        const animationDuration = 500;
+        const activeTokens = this.actor.system.partyMembers.flatMap(member => member.getActiveTokens());
+        const { x: actorX, y: actorY } = this.actor.token;
+        if (activeTokens.length > 0) {
+            for (let token of activeTokens) {
+                await token.document.update(
+                    { x: actorX, y: actorY, alpha: 0 },
+                    { animation: { duration: animationDuration } }
+                );
+                setTimeout(() => token.document.delete(), animationDuration);
+            }
+        } else {
+            const activeScene = game.scenes.find(x => x.active);
+            const newTokens = await activeScene.createEmbeddedDocuments(
+                'Token',
+                this.actor.system.partyMembers.map(member => ({
+                    ...member.getTokenDocument(),
+                    actorId: member.id,
+                    actorLink: true,
+                    alpha: 0,
+                    x: actorX,
+                    y: actorY
+                }))
+            );
+
+            const { sizeX, sizeY } = activeScene.grid;
+            const nrRandomPositions = Math.ceil(newTokens.length / 8) * 8;
+            /* This is an overcomplicated mess, but I'm stupid */
+            const positions = shuffleArray(
+                [...Array(nrRandomPositions).keys()].map((_, index) => {
+                    const nonZeroIndex = index + 1;
+                    const indexFloor = Math.floor(index / 8);
+                    const distanceCoefficient = indexFloor + 1;
+                    const side = 3 + indexFloor * 2;
+                    const sideMiddle = Math.ceil(side / 2);
+                    const inbetween = 1 + indexFloor * 2;
+                    const inbetweenMiddle = Math.ceil(inbetween / 2);
+
+                    if (index < side) {
+                        const distance =
+                            nonZeroIndex === sideMiddle
+                                ? 0
+                                : nonZeroIndex < sideMiddle
+                                  ? -nonZeroIndex
+                                  : nonZeroIndex - sideMiddle;
+                        return { x: actorX - sizeX * distance, y: actorY - sizeY * distanceCoefficient };
+                    } else if (index < side + inbetween) {
+                        const inbetweenIndex = nonZeroIndex - side;
+                        const distance =
+                            inbetweenIndex === inbetweenMiddle
+                                ? 0
+                                : inbetweenIndex < inbetweenMiddle
+                                  ? -inbetweenIndex
+                                  : inbetweenIndex - inbetweenMiddle;
+                        return { x: actorX + sizeX * distanceCoefficient, y: actorY + sizeY * distance };
+                    } else if (index < 2 * side + inbetween) {
+                        const sideIndex = nonZeroIndex - side - inbetween;
+                        const distance =
+                            sideIndex === sideMiddle
+                                ? 0
+                                : sideIndex < sideMiddle
+                                  ? sideIndex
+                                  : -(sideIndex - sideMiddle);
+                        return { x: actorX + sizeX * distance, y: actorY + sizeY * distanceCoefficient };
+                    } else {
+                        const inbetweenIndex = nonZeroIndex - 2 * side - inbetween;
+                        const distance =
+                            inbetweenIndex === inbetweenMiddle
+                                ? 0
+                                : inbetweenIndex < inbetweenMiddle
+                                  ? inbetweenIndex
+                                  : -(inbetweenIndex - inbetweenMiddle);
+                        return { x: actorX - sizeX * distanceCoefficient, y: actorY + sizeY * distance };
+                    }
+                })
+            );
+
+            for (let token of newTokens) {
+                const position = positions.pop();
+                token.update(
+                    { x: position.x, y: position.y, alpha: 1 },
+                    { animation: { duration: animationDuration } }
+                );
+            }
         }
     }
 
