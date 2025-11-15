c14f50a50c9062e0ea9dcc927a90552df4a7757f
WBHarry <williambjrklund@gmail.com>
2025-11-15 15:55:59 +0100

    Fixed so lists do not fail to show items when search is empty

--- module/applications/ui/itemBrowser.mjs
+++ module/applications/ui/itemBrowser.mjs
@@ -430,7 +430,7 @@ export class ItemBrowser extends HandlebarsApplicationMixin(ApplicationV2) {
             if (matchesMenu) this.#filteredItems.browser.input.add(item.id);
 
             const { search } = this.#filteredItems.browser;
-            li.hidden = !(search.has(item.id) && matchesMenu);
+            li.hidden = !((this.#search.browser.query.length === 0 || search.has(item.id)) && matchesMenu);
         }
     }
 
