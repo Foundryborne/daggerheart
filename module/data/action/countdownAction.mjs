69853fd9d670ad3b64d32142d2c11c08764ad083
WBHarry <williambjrklund@gmail.com>
2025-11-20 11:09:43 +0100

    Added initial countdown upon creating a CountdownAction

--- module/data/action/countdownAction.mjs
+++ module/data/action/countdownAction.mjs
@@ -12,4 +12,18 @@ export default class DhCountdownAction extends DHBaseAction {
             }
         };
     }
+
+    /** @inheritdoc */
+    static getSourceConfig(parent) {
+        const updateSource = game.system.api.data.actions.actionsTypes.base.getSourceConfig(parent);
+        updateSource['countdown'] = [
+            {
+                ...game.system.api.data.countdowns.DhCountdown.defaultCountdown(),
+                name: parent.parent.name,
+                img: parent.parent.img
+            }
+        ];
+
+        return updateSource;
+    }
 }
