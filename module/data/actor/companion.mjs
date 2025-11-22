eb573e7ccb01b0c15f4e21d2189f1ab3072a498f
WBHarry <williambjrklund@gmail.com>
2025-11-22 21:43:31 +0100

    Added implementation of unstoppable

--- module/data/actor/companion.mjs
+++ module/data/actor/companion.mjs
@@ -51,6 +51,12 @@ export default class DhCompanion extends BaseDataActor {
                     }
                 }
             ),
+            rules: new fields.SchemaField({
+                conditionImmunities: new fields.SchemaField({
+                    vulnerable: new fields.BooleanField({ initial: false }),
+                    restrained: new fields.BooleanField({ initial: false })
+                })
+            }),
             attack: new ActionField({
                 initial: {
                     name: 'Attack',
