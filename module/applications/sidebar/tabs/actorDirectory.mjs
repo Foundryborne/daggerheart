24ec7673b589b50fcb711a7157bcf9dd4192c984
Carlos Fernandez <cfern1990@gmail.com>
2025-11-25 06:45:13 -0500

    Show adversary and environment type

--- module/applications/sidebar/tabs/actorDirectory.mjs
+++ module/applications/sidebar/tabs/actorDirectory.mjs
@@ -1,11 +1,19 @@
 export default class DhActorDirectory extends foundry.applications.sidebar.tabs.ActorDirectory {
     static DEFAULT_OPTIONS = {
-        renderUpdateKeys: [
-            "system.levelData.level.current",
-            "system.partner",
-            "system.tier"
-        ]
-    }
+        renderUpdateKeys: ['system.levelData.level.current', 'system.partner', 'system.tier']
+    };
 
-    static _entryPartial = "systems/daggerheart/templates/ui/sidebar/actor-document-partial.hbs";
+    static _entryPartial = 'systems/daggerheart/templates/ui/sidebar/actor-document-partial.hbs';
+
+    async _prepareDirectoryContext(context, options) {
+        await super._prepareDirectoryContext(context, options);
+        const adversaryTypes = CONFIG.DH.ACTOR.allAdversaryTypes();
+        context.getTypeLabel = document => {
+            return document.type === 'adversary'
+                ? game.i18n.localize(adversaryTypes[document.system.type].label)
+                : document.type === 'environment'
+                  ? game.i18n.localize(CONFIG.DH.ACTOR.environmentTypes[document.system.type].label)
+                  : null;
+        };
+    }
 }
