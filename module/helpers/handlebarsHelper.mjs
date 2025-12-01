51135b6a679436bf08e320689d1e06d2055b4bb8
WBHarry <williambjrklund@gmail.com>
2025-12-01 16:25:59 +0100

    Fixed so the damage calculation is correct. Fixed so the right tooltip is shown for attacks

--- module/helpers/handlebarsHelper.mjs
+++ module/helpers/handlebarsHelper.mjs
@@ -35,14 +35,8 @@ export default class RegisterHandlebarsHelpers {
         return accum;
     }
 
-    static damageFormula(attack, actor) {
-        const traitTotal = actor.system.traits?.[attack.roll.trait]?.value;
-        const instances = [
-            attack.damage.parts.map(x => Roll.replaceFormulaData(x.value.getFormula(), actor)).join(' + '),
-            traitTotal
-        ].filter(x => x);
-
-        return instances.join(traitTotal > 0 ? ' + ' : ' - ');
+    static damageFormula(attack) {
+        return game.system.api.fields.ActionFields.DamageField.getFormulaString.bind(attack)(attack.damage.parts);
     }
 
     static formulaValue(formula, item) {
