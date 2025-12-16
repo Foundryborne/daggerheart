71b40b1189e721b5a795d4942b9dd93397b0a1f7
Carlos Fernandez <cfern1990@gmail.com>
2025-12-16 01:57:26 -0500

    Support drag dropping currencies to actor sheets

--- module/applications/sheets/api/base-actor.mjs
+++ module/applications/sheets/api/base-actor.mjs
@@ -34,7 +34,10 @@ export default class DHBaseActorSheet extends DHApplicationMixin(ActorSheetV2) {
                 }
             }
         ],
-        dragDrop: [{ dragSelector: '.inventory-item[data-type="attack"]', dropSelector: null }]
+        dragDrop: [
+            { dragSelector: '.inventory-item[data-type="attack"]', dropSelector: null },
+            { dragSelector: ".currency[data-currency] .drag-handle", dropSelector: null }
+        ]
     };
 
     /* -------------------------------------------- */
@@ -254,14 +257,67 @@ export default class DHBaseActorSheet extends DHApplicationMixin(ActorSheetV2) {
     /*  Application Drag/Drop                       */
     /* -------------------------------------------- */
 
+    async #determineTransferQuantity({ originActor, item, currency }) {
+        originActor ??= item?.actor;
+        const max = item?.system.quantity ?? originActor.system.gold[currency] ?? null;
+        if (!max || max === 0) return null;
+        if (max === 1) return max;
+
+        const homebrewKey = CONFIG.DH.SETTINGS.gameSettings.Homebrew;
+        const currencySetting = game.settings.get(CONFIG.DH.id, homebrewKey).currency?.[currency] ?? null;
+        const name = item?.name ?? currencySetting?.label;
+
+        return foundry.applications.api.DialogV2.input({
+            classes: ['daggerheart', 'dh-style', "item-transfer"],
+            content: await foundry.applications.handlebars.renderTemplate(
+                "systems/daggerheart/templates/dialogs/item-transfer.hbs",
+                {
+                    originActor: originActor,
+                    targetActor: this.document,
+                    itemImage: item?.img,
+                    currencyIcon: currencySetting?.icon,
+                    max,
+                }
+            ),
+            window: {
+                title: name,
+                icon: 'fa-solid fa-hand-holding-hand'
+            },
+            position: {
+                width: 400,
+            },
+            ok: {
+                label: game.i18n.localize("DAGGERHEART.APPLICATIONS.ItemTransfer.transfer"),
+                callback: (_, button) => button.form.elements.quantity.value || null,
+            }
+        });
+    }
+
+    async _onDrop(event) {
+        event.stopPropagation();
+        const data = foundry.applications.ux.TextEditor.implementation.getDragEventData(event);
+        if (data.type === 'Currency' && ['character', 'party'].includes(this.document.type)) {
+            const originActor = await foundry.utils.fromUuid(data.originActor);
+            if (!originActor || originActor.uuid === this.document.uuid) return;
+            const currency = data.currency;
+            const quantity = await this.#determineTransferQuantity({ originActor, currency });
+            if (quantity) {
+                originActor.update({ [`system.gold.${currency}`]: Math.max(0, originActor.system.gold[currency] - quantity) });
+                this.document.update({ [`system.gold.${currency}`]: this.document.system.gold[currency] + quantity });
+            }
+            return;
+        }
+
+        return super._onDrop(event);
+    }
+
     async _onDropItem(event, item) {
         const data = foundry.applications.ux.TextEditor.implementation.getDragEventData(event);
-        const physicalActorTypes = ['character', 'party'];
         const originActor = item.actor;
         if (
             item.actor?.uuid === this.document.uuid ||
             !originActor ||
-            !physicalActorTypes.includes(this.document.type)
+            !['character', 'party'].includes(this.document.type)
         ) {
             return super._onDropItem(event, item);
         }
@@ -270,10 +326,7 @@ export default class DHBaseActorSheet extends DHApplicationMixin(ActorSheetV2) {
         if (item.system.metadata.isInventoryItem) {
             if (item.system.metadata.isQuantifiable) {
                 const actorItem = originActor.items.get(data.originId);
-                const quantityTransfered =
-                    actorItem.system.quantity === 1
-                        ? 1
-                        : await game.system.api.applications.dialogs.ItemTransferDialog.configure(item);
+                const quantityTransfered = await this.#determineTransferQuantity({ item });
 
                 if (quantityTransfered) {
                     if (quantityTransfered === actorItem.system.quantity) {
@@ -314,6 +367,16 @@ export default class DHBaseActorSheet extends DHApplicationMixin(ActorSheetV2) {
      * @param {DragEvent} event - The drag event
      */
     async _onDragStart(event) {
+        // Handle drag/dropping currencies
+        const currencyEl = event.currentTarget.closest(".currency[data-currency]");
+        if (currencyEl) {
+            const currency = currencyEl.dataset.currency;
+            const data = { type: 'Currency', currency, originActor: this.document.uuid };
+            event.dataTransfer.setData('text/plain', JSON.stringify(data));
+            return;
+        }
+
+        // Handle drag/dropping attacks
         const attackItem = event.currentTarget.closest('.inventory-item[data-type="attack"]');
         if (attackItem) {
             const attackData = {
