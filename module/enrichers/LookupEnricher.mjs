6011255a1c5747b5d5703002b053fcb7747dbb9b
WBHarry <williambjrklund@gmail.com>
2025-12-25 01:25:57 +0100

    Fixed so that lookupEnricher's fallback state is securely returning the lookup string

--- module/enrichers/LookupEnricher.mjs
+++ module/enrichers/LookupEnricher.mjs
@@ -1,8 +1,14 @@
 import { parseInlineParams } from './parser.mjs';
 
 export default function DhLookupEnricher(match, { rollData }) {
-    const results = parseInlineParams(match[1], { first: 'formula' });
+    const lookupParam = match[1];
     const element = document.createElement('span');
-    element.textContent = Roll.replaceFormulaData(String(results.formula), rollData);
+    element.textContent = match[0];
+    if (rollData && lookupParam) {
+        const results = parseInlineParams(match[1], { first: 'formula' });
+        const text = Roll.replaceFormulaData(String(results.formula), rollData);
+        element.textContent = text !== lookupParam ? text : element.textContent;
+    }
+
     return element;
 }
