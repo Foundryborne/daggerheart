84b28bd9c90308ff357a94730ae6cebeefbd29d9
Nikhil Nagarajan <potter.nikhil@gmail.com>
2025-12-27 10:21:17 -0500

    Schema definition for DHSummonAction

--- module/data/action/summonAction.mjs
+++ module/data/action/summonAction.mjs
@@ -1,3 +1,4 @@
+import { ui } from '../../applications/_module.mjs';
 import DHBaseAction from './baseAction.mjs';
 
 export default class DHSummonAction extends DHBaseAction {
@@ -5,12 +6,18 @@ export default class DHSummonAction extends DHBaseAction {
         const fields = foundry.data.fields;
         return {
             ...super.defineSchema(),
-            documentUUID: new fields.DocumentUUIDField({ type: 'Actor' })
+            tokenArray: new fields.ArrayField(new fields.SchemaField({
+                actorUUID: new fields.DocumentUUIDField({ type: 'Actor', required: true }),
+                count: new fields.NumberField({ required: true, default: 1, min:1 })
+            }), { required: false })
         };
     }
 
     async trigger(event, ...args) {
-        if (!this.canSummon || !canvas.scene) return;
+        if (!this.canSummon || !canvas.scene){
+            ui.notifications.error(game.i18n.localize("DAGGERHEART.ACTIONS.TYPES.summon.error"));
+            return;
+        }
     }
 
     get canSummon() {
