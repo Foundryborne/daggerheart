ec0b0df02ed72ba37e63f23286bfc81d501896fa
WBHarry <williambjrklund@gmail.com>
2026-01-03 12:13:43 +0100

    .

--- module/applications/scene/sceneConfigSettings.mjs
+++ module/applications/scene/sceneConfigSettings.mjs
@@ -98,11 +98,9 @@ export default class DhSceneConfigSettings extends foundry.applications.sheets.S
     /** @override */
     async _processSubmitData(event, form, submitData, options) {
         submitData.flags.daggerheart = this.daggerheartFlag.toObject();
-        if (this.document._source.flags.daggerheart) {
-            for (const key of Object.keys(this.document._source.flags.daggerheart.sceneEnvironments)) {
-                if (!submitData.flags.daggerheart.sceneEnvironments[key]) {
-                    submitData.flags.daggerheart.sceneEnvironments[`-=${key}`] = null;
-                }
+        for (const key of Object.keys(this.document._source.flags.daggerheart?.sceneEnvironments ?? {})) {
+            if (!submitData.flags.daggerheart.sceneEnvironments[key]) {
+                submitData.flags.daggerheart.sceneEnvironments[`-=${key}`] = null;
             }
         }
 
