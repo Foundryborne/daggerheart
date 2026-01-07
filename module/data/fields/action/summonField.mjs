f5e00dedb2265a18df756c95ecc9b0c7a4eed540
WBHarry <williambjrklund@gmail.com>
2026-01-07 19:14:17 +0100

    .

--- module/data/fields/action/summonField.mjs
+++ module/data/fields/action/summonField.mjs
@@ -7,32 +7,22 @@ export default class DHSummonField extends fields.ArrayField {
     static order = 120;
 
     constructor(options = {}, context = {}) {
-            const summonFields = new fields.SchemaField({
-                actorUUID: new fields.DocumentUUIDField({
-                    type: 'Actor',
-                    required: true
-                }),
-                count: new fields.NumberField({
-                    required: true,
-                    default: 1,
-                    min: 1,
-                    integer: true
-                })
+        const summonFields = new fields.SchemaField({
+            actorUUID: new fields.DocumentUUIDField({
+                type: 'Actor',
+                required: true
+            }),
+            count: new fields.NumberField({
+                required: true,
+                default: 1,
+                min: 1,
+                integer: true
             })
+        });
         super(summonFields, options, context);
-            
-        //     summon: new fields.ArrayField(new fields.SchemaField({
-        //         actorUUID: new fields.DocumentUUIDField({ 
-        //             type: 'Actor', 
-        //             required: true }),
-        //         count: new fields.NumberField({ 
-        //             required: true, 
-        //             default: 1, 
-        //             min: 1, 
-        //             integer: true })
-        //     }), { required: false, initial: [] })
-        // };
-        // super(summonFields, options, context);
     }
 
+    static async execute(config) {
+        console.log('something');
+    }
 }
