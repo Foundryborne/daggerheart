0b4266ba089a2614184845cc2fea8c697ecbc94f
WBHarry <williambjrklund@gmail.com>
2026-02-02 10:50:18 +0100

    Revert "Initial chat message description design change"
    This reverts commit f4f5fd6c24d05c6967615fe8498a34aee739b1b3.

--- module/documents/chatMessage.mjs
+++ module/documents/chatMessage.mjs
@@ -101,7 +101,7 @@ export default class DhpChatMessage extends foundry.documents.ChatMessage {
                     CONFIG.DH.id,
                     CONFIG.DH.SETTINGS.gameSettings.appearance
                 ).expandRollMessage,
-                rollSections = html.querySelectorAll('.roll-part, .description-section'),
+                rollSections = html.querySelectorAll('.roll-part'),
                 itemDesc = html.querySelector('.domain-card-move');
             rollSections.forEach(s => {
                 if (s.classList.contains('roll-section')) {
