46d60f4b7bf06d8446bab1c674af32d3a8e73820
WBHarry <williambjrklund@gmail.com>
2026-02-06 11:25:16 +0100

    Actually nice solution instead O_O

--- module/applications/sheets-configs/activeEffectConfig.mjs
+++ module/applications/sheets-configs/activeEffectConfig.mjs
@@ -4,20 +4,34 @@ export default class DhActiveEffectConfig extends foundry.applications.sheets.Ac
     constructor(options) {
         super(options);
 
-        const ignoredActorKeys = ['config', 'DhEnvironment'];
+        const ignoredActorKeys = ['config', 'DhEnvironment', 'DhParty'];
         this.changeChoices = Object.keys(game.system.api.models.actors).reduce((acc, key) => {
-            if (!ignoredActorKeys.includes(key)) {
-                const model = game.system.api.models.actors[key];
-                const attributes = CONFIG.Token.documentClass.getTrackedAttributes(model);
-                // As per DHToken._getTrackedAttributesFromSchema, attributes.bar have a max version as well.
-                const maxAttributes = attributes.bar.map(x => [...x, 'max']);
-                attributes.value.push(...maxAttributes);
-                const group = game.i18n.localize(model.metadata.label);
-                const choices = CONFIG.Token.documentClass
-                    .getTrackedAttributeChoices(attributes, model)
-                    .map(x => ({ ...x, group: group }));
-                acc.push(...choices);
-            }
+            if (ignoredActorKeys.includes(key)) return acc;
+
+            const model = game.system.api.models.actors[key];
+            const group = game.i18n.localize(model.metadata.label);
+            const attributes = CONFIG.Token.documentClass.getTrackedAttributes(model.metadata.type);
+
+            const getLabel = path => {
+                const label = model.schema.getField(path)?.label;
+                return label ? game.i18n.localize(label) : path;
+            };
+
+            const bars = attributes.bar.flatMap(x => {
+                const joined = `${x.join('.')}.max`;
+                const label =
+                    joined === 'resources.hope.max'
+                        ? 'DAGGERHEART.SETTINGS.Homebrew.FIELDS.maxHope.label'
+                        : getLabel(joined);
+                return { value: joined, label, group };
+            });
+            const values = attributes.value.flatMap(x => {
+                const joined = x.join('.');
+                return { value: joined, label: getLabel(joined), group };
+            });
+
+            acc.push(...bars, ...values);
+
             return acc;
         }, []);
     }
