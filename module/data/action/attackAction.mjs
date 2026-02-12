f452adec00c087b6962b09f9687ecd030b0be7cf
WBHarry <williambjrklund@gmail.com>
2026-02-12 18:47:42 +0100

    Changed to using lookup for Group Attack damage

--- module/data/action/attackAction.mjs
+++ module/data/action/attackAction.mjs
@@ -34,6 +34,13 @@ export default class DHAttackAction extends DHDamageAction {
         };
     }
 
+    get damageFormula() {
+        const hitPointsPart = this.damage.parts.find(x => x.applyTo === CONFIG.DH.GENERAL.healingTypes.hitPoints.id);
+        if (!hitPointsPart) return '0';
+
+        return hitPointsPart.value.getFormula();
+    }
+
     async use(event, options) {
         const result = await super.use(event, options);
         if (!result.message) return;
