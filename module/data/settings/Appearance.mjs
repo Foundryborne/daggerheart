f75468c9e01f111f29cd215db1208f01d40c9b9d
WBHarry <williambjrklund@gmail.com>
2026-03-07 18:26:56 +0100

    Added an AppearanceSetting that decided if players should get to see information about hit/miss in chat messages

--- module/data/settings/Appearance.mjs
+++ module/data/settings/Appearance.mjs
@@ -25,6 +25,7 @@ export default class DhAppearance extends foundry.abstract.DataModel {
                 choices: CONFIG.DH.GENERAL.fearDisplay,
                 initial: CONFIG.DH.GENERAL.fearDisplay.token.value
             }),
+            hideHitMissForPlayers: new BooleanField({ initial: false }),
             displayCountdownUI: new BooleanField({ initial: true }),
             diceSoNice: new SchemaField({
                 hope: diceStyle({ fg: '#ffffff', bg: '#ffe760', outline: '#000000', edge: '#ffffff' }),
