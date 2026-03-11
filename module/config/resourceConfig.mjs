97c91e040a0ada6e1b3fa1b8100945e7d6543b83
Carlos Fernandez <CarlosFdez@users.noreply.github.com>
2026-03-11 05:56:14 -0400

    Use custom config for module data instead of including in all (#1724)
    * Use custom config for module data instead of including in all
    
    * More simple

--- module/config/resourceConfig.mjs
+++ module/config/resourceConfig.mjs
@@ -71,15 +71,18 @@ const companionBaseResources = Object.freeze({
 
 export const character = {
     base: characterBaseResources,
+    custom: {}, // module stuff goes here
     all: { ...characterBaseResources },
 };
 
 export const adversary = {
     base: adversaryBaseResources,
+    custom: {}, // module stuff goes here
     all: { ...adversaryBaseResources },
 };
 
 export const companion = {
     base: companionBaseResources,
+    custom: {}, // module stuff goes here
     all: { ...companionBaseResources },
 };
