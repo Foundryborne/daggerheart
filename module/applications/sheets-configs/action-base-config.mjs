4a116a62ee97744d1245eb3a3f7ea9fdb50ef1ee
WBHarry <williambjrklund@gmail.com>
2026-03-23 17:13:27 +0100

    Fixed ActionBaseConfig error when no damage present on the action

--- module/applications/sheets-configs/action-base-config.mjs
+++ module/applications/sheets-configs/action-base-config.mjs
@@ -154,8 +154,13 @@ export default class DHActionBaseConfig extends DaggerheartSheet(ApplicationV2) 
         context.openSection = this.openSection;
         context.tabs = this._getTabs(this.constructor.TABS);
         context.config = CONFIG.DH;
-        if (this.action.damage?.hasOwnProperty('includeBase') && this.action.type === 'attack')
-            context.hasBaseDamage = !!this.action.parent.attack;
+        if (this.action.hasDamage) {
+            context.allDamageTypesUsed = !getUnusedDamageTypes(this.action.damage.parts).length;
+
+            if (this.action.damage.hasOwnProperty('includeBase') && this.action.type === 'attack')
+                context.hasBaseDamage = !!this.action.parent.attack;
+        }
+
         context.costOptions = this.getCostOptions();
         context.getRollTypeOptions = this.getRollTypeOptions();
         context.disableOption = this.disableOption.bind(this);
@@ -173,7 +178,6 @@ export default class DHActionBaseConfig extends DaggerheartSheet(ApplicationV2) 
                 revealed: this.openTrigger === index
             };
         });
-        context.allDamageTypesUsed = !getUnusedDamageTypes(this.action.damage.parts).length;
 
         const settingsTiers = game.settings.get(CONFIG.DH.id, CONFIG.DH.SETTINGS.gameSettings.LevelTiers).tiers;
         context.tierOptions = [
