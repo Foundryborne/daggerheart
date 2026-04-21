89c02faf0e51ab1003ea253ed3e8ee5d9aaae2b4
Carlos Fernandez <cfern1990@gmail.com>
2026-04-21 18:30:00 -0400

    Redesign group roll dialog

--- module/applications/dialogs/groupRollDialog.mjs
+++ module/applications/dialogs/groupRollDialog.mjs
@@ -37,16 +37,16 @@ export default class GroupRollDialog extends HandlebarsApplicationMixin(Applicat
         tag: 'form',
         id: 'GroupRollDialog',
         classes: ['daggerheart', 'views', 'dh-style', 'dialog', 'group-roll-dialog'],
-        position: { width: 550, height: 'auto' },
+        position: { width: 380, height: 'auto' },
+        window: {
+            icon: 'fa-solid fa-users'
+        },
         actions: {
             toggleSelectMember: this.#toggleSelectMember,
             startGroupRoll: this.#startGroupRoll,
             makeRoll: this.#makeRoll,
             removeRoll: this.#removeRoll,
             rerollDice: this.#rerollDice,
-            makeLeaderRoll: this.#makeLeaderRoll,
-            removeLeaderRoll: this.#removeLeaderRoll,
-            rerollLeaderDice: this.#rerollLeaderDice,
             markSuccessfull: this.#markSuccessfull,
             cancelRoll: this.#onCancelRoll,
             finishRoll: this.#finishRoll
@@ -59,17 +59,21 @@ export default class GroupRollDialog extends HandlebarsApplicationMixin(Applicat
             id: 'initialization',
             template: 'systems/daggerheart/templates/dialogs/groupRollDialog/initialization.hbs'
         },
+        main: {
+            id: 'main',
+            template: 'systems/daggerheart/templates/dialogs/groupRollDialog/main.hbs'
+        },
         leader: {
             id: 'leader',
-            template: 'systems/daggerheart/templates/dialogs/groupRollDialog/leader.hbs'
+            template: 'systems/daggerheart/templates/dialogs/groupRollDialog/parts/member.hbs'
         },
-        groupRoll: {
-            id: 'groupRoll',
-            template: 'systems/daggerheart/templates/dialogs/groupRollDialog/groupRoll.hbs'
+        result: {
+            id: 'result',
+            template: 'systems/daggerheart/templates/dialogs/groupRollDialog/parts/result.hbs'
         },
         footer: {
             id: 'footer',
-            template: 'systems/daggerheart/templates/dialogs/groupRollDialog/footer.hbs'
+            template: 'systems/daggerheart/templates/dialogs/groupRollDialog/parts/footer.hbs'
         }
     };
 
@@ -89,40 +93,14 @@ export default class GroupRollDialog extends HandlebarsApplicationMixin(Applicat
     }
 
     _configureRenderParts(options) {
-        const { initialization, leader, groupRoll, footer } = super._configureRenderParts(options);
-        const augmentedParts = { initialization };
+        const parts = super._configureRenderParts(options);
         for (const memberKey of Object.keys(this.party.system.groupRoll.aidingCharacters)) {
-            augmentedParts[memberKey] = {
+            parts[memberKey] = {
                 id: memberKey,
-                template: 'systems/daggerheart/templates/dialogs/groupRollDialog/groupRollMember.hbs'
+                template: 'systems/daggerheart/templates/dialogs/groupRollDialog/parts/member.hbs'
             };
         }
-
-        augmentedParts.leader = leader;
-        augmentedParts.groupRoll = groupRoll;
-        augmentedParts.footer = footer;
-
-        return augmentedParts;
-    }
-
-    /**@inheritdoc */
-    async _onRender(context, options) {
-        await super._onRender(context, options);
-
-        if (this.element.querySelector('.team-container')) return;
-
-        if (this.tabGroups.application !== this.constructor.PARTS.initialization.id) {
-            const initializationPart = this.element.querySelector('.initialization-container');
-            initializationPart.insertAdjacentHTML('afterend', '<div class="team-container"></div>');
-            initializationPart.insertAdjacentHTML(
-                'afterend',
-                `<div class="section-title">${game.i18n.localize('DAGGERHEART.APPLICATIONS.GroupRollSelect.aidingCharacters')}</div>`
-            );
-
-            const teamContainer = this.element.querySelector('.team-container');
-            for (const memberContainer of this.element.querySelectorAll('.team-member-container'))
-                teamContainer.appendChild(memberContainer);
-        }
+        return parts;
     }
 
     async _prepareContext(_options) {
@@ -134,6 +112,7 @@ export default class GroupRollDialog extends HandlebarsApplicationMixin(Applicat
         context.data = this.party.system.groupRoll;
         context.traitOptions = CONFIG.DH.ACTOR.abilities;
         context.members = {};
+        context.aidKeys = Object.keys(this.party.system.groupRoll.aidingCharacters);
         context.allHaveRolled = Object.keys(context.data.participants).every(key => {
             const data = context.data.participants[key];
             return Boolean(data.rollData);
@@ -145,6 +124,7 @@ export default class GroupRollDialog extends HandlebarsApplicationMixin(Applicat
     async _preparePartContext(partId, context, options) {
         const partContext = await super._preparePartContext(partId, context, options);
         partContext.partId = partId;
+        partContext.leader = this.getRollCharacterData(this.party.system.groupRoll.leader);
 
         switch (partId) {
             case 'initialization':
@@ -162,10 +142,7 @@ export default class GroupRollDialog extends HandlebarsApplicationMixin(Applicat
                 partContext.canStartGroupRoll = selectedMembers.length > 1 && this.leader?.memberId;
                 partContext.openForAllPlayers = this.openForAllPlayers;
                 break;
-            case 'leader':
-                partContext.leader = this.getRollCharacterData(this.party.system.groupRoll.leader);
-                break;
-            case 'groupRoll':
+            case 'result':
                 const leader = this.party.system.groupRoll.leader;
                 partContext.hasRolled =
                     leader?.rollData ||
@@ -216,20 +193,36 @@ export default class GroupRollDialog extends HandlebarsApplicationMixin(Applicat
         if (!data) return {};
 
         const actor = game.actors.get(data.id);
+        const isLeader = data === this.party.system.groupRoll.leader;
 
         return {
             ...data,
+            type: isLeader ? 'leader' : 'aid',
+            basePath: isLeader ? 'system.groupRoll.leader' : `system.groupRoll.aidingCharacters.${data.id}`,
+            rollChoiceLabel: _loc(CONFIG.DH.ACTOR.abilities[data.rollChoice]?.label),
             roll: data.roll,
-            isEditable: actor.testUserPermission(game.user, CONST.DOCUMENT_OWNERSHIP_LEVELS.OWNER),
+            isEditable: actor?.testUserPermission(game.user, CONST.DOCUMENT_OWNERSHIP_LEVELS.OWNER),
             key: partId,
             readyToRoll: Boolean(data.rollChoice),
             hasRolled: Boolean(data.rollData)
         };
     }
 
+    #getCharacterDataById(id) {
+        if (!id) return null;
+
+        const groupRoll = this.party.system.groupRoll;
+        if (id === 'leader' || id === groupRoll.leader?.id) {
+            return { data: groupRoll.leader, basePath: 'system.groupRoll.leader' };
+        } else if (id in groupRoll.aidingCharacters) {
+            return { data: groupRoll.aidingCharacters[id], basePath: `system.groupRoll.aidingCharacters.${id}` };
+        }
+
+        return null;
+    }
+
     static async updateData(event, _, formData) {
         const partyData = foundry.utils.expandObject(formData.object);
-
         this.updatePartyData(partyData, this.getUpdatingParts(event.target));
     }
 
@@ -256,16 +249,16 @@ export default class GroupRollDialog extends HandlebarsApplicationMixin(Applicat
     }
 
     getUpdatingParts(target) {
-        const { initialization, leader, groupRoll, footer } = this.constructor.PARTS;
+        const { initialization, leader, result, footer } = this.constructor.PARTS;
         const isInitialization = this.tabGroups.application === initialization.id;
-        const updatingMember = target.closest('.team-member-container')?.dataset?.memberKey;
-        const updatingLeader = target.closest('.main-character-outer-container');
+        const updatingMember = target.closest('.member-roll-container.aid')?.dataset?.memberKey;
+        const updatingLeader = target.closest('.member-roll-container.leader');
 
         return [
             ...(isInitialization ? [initialization.id] : []),
             ...(updatingMember ? [updatingMember] : []),
             ...(updatingLeader ? [leader.id] : []),
-            ...(!isInitialization ? [groupRoll.id, footer.id] : [])
+            ...(!isInitialization ? [result.id, footer.id] : [])
         ];
     }
 
@@ -368,21 +361,18 @@ export default class GroupRollDialog extends HandlebarsApplicationMixin(Applicat
         );
     }
 
+    /** @this GroupRollDialog */
     static async #makeRoll(_event, button) {
-        const { member } = button.dataset;
-        const character = this.party.system.groupRoll.aidingCharacters[member];
-        this.makeRoll(button, character, `system.groupRoll.aidingCharacters.${member}.rollData`);
+        const { data, basePath } = this.#getCharacterDataById(button.dataset.member);
+        this.makeRoll(button, data, `${basePath}.rollData`);
     }
 
-    static async #makeLeaderRoll(_event, button) {
-        const character = this.party.system.groupRoll.leader;
-        this.makeRoll(button, character, 'system.groupRoll.leader.rollData');
-    }
-
-    async removeRoll(button, path) {
+    /** @this GroupRollDialog  */
+    static async #removeRoll(_event, button) {
+        const { basePath } = this.#getCharacterDataById(button.dataset.member);
         this.updatePartyData(
             {
-                [path]: {
+                [basePath]: {
                     rollData: null,
                     rollChoice: null,
                     selected: false,
@@ -393,16 +383,10 @@ export default class GroupRollDialog extends HandlebarsApplicationMixin(Applicat
         );
     }
 
-    static async #removeRoll(_event, button) {
-        this.removeRoll(button, `system.groupRoll.aidingCharacters.${button.dataset.member}`);
-    }
-
-    static async #removeLeaderRoll(_event, button) {
-        this.removeRoll(button, 'system.groupRoll.leader');
-    }
-
-    async rerollDice(button, data, path) {
+    /** @this GroupRollDialog */
+    static async #rerollDice(_, button) {
         const { diceType } = button.dataset;
+        const { data, basePath } = this.#getCharacterDataById(button.dataset.member);
 
         const dieIndex = diceType === 'hope' ? 0 : diceType === 'fear' ? 1 : 2;
         const newRoll = game.system.api.dice.DualityRoll.fromData(data.rollData);
@@ -416,23 +400,10 @@ export default class GroupRollDialog extends HandlebarsApplicationMixin(Applicat
         const rollData = newRoll.toJSON();
         this.updatePartyData(
             {
-                [path]: rollData
+                [`${basePath}.rollData`]: rollData
             },
             this.getUpdatingParts(button)
         );
-    }
-
-    static async #rerollDice(_, button) {
-        const { member } = button.dataset;
-        this.rerollDice(
-            button,
-            this.party.system.groupRoll.aidingCharacters[member],
-            `system.groupRoll.aidingCharacters.${member}.rollData`
-        );
-    }
-
-    static async #rerollLeaderDice(_, button) {
-        this.rerollDice(button, this.party.system.groupRoll.leader, `system.groupRoll.leader.rollData`);
     }
 
     static #markSuccessfull(_event, button) {
