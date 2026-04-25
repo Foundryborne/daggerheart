0ccf3bde86d604f7d7d3ee7a89b3b69712dc4fc5
Carlos Fernandez <cfern1990@gmail.com>
2026-04-25 18:01:59 -0400

    Add proxy trap for the in operator

--- module/helpers/utils.mjs
+++ module/helpers/utils.mjs
@@ -373,9 +373,10 @@ export const itemAbleRollParse = (value, actor, item) => {
     const isItemTarget = value.toLowerCase().includes('item.@');
     const slicedValue = isItemTarget ? value.replaceAll(/item\.@/gi, '@') : value;
     const model = isItemTarget || item instanceof Item ? item : actor;
+    const rollData = isItemTarget || !model?.getRollData ? model : model.getRollData();
 
     try {
-        return Roll.replaceFormulaData(slicedValue, isItemTarget || !model?.getRollData ? model : model.getRollData());
+        return Roll.replaceFormulaData(slicedValue, rollData);
     } catch (_) {
         return '';
     }
@@ -831,6 +832,9 @@ export function createShallowProxy(obj) {
         deleteProperty(_target, prop) {
             delete overrides[prop];
             return true;
+        },
+        has(target, key) {
+            return key in overrides || key in target;
         }
     });
 }
