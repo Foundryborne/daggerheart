9f3ffdbdbb33bee01e5a5b038413c739f7d3a547
WBHarry <williambjrklund@gmail.com>
2026-06-08 16:16:11 +0200

    Improved handling so that modules changing the game pause work normally

--- module/applications/ui/gamePause.mjs
+++ module/applications/ui/gamePause.mjs
@@ -1,9 +1,22 @@
 export default class DhGamePause extends foundry.applications.ui.GamePause {
+    async _onRender(context, options) {
+        await super._onRender(context, options);
+    
+        /* Avoid altering the styling if a module has subscribed to the renderGamePause hook  */
+        if (!Hooks.events.renderGamePause?.length) {
+            this.element.classList.add('dh-style');
+        }
+    }
+
     /** @override */
     async _prepareContext(options) {
         const context = await super._prepareContext(options);
-        context.spin = false;
-        context.icon = 'systems/daggerheart/assets/logos/compatible_with_DH_logos-10.png';
+
+        /* Avoid altering the gamepause context if a module has subscribed to the renderGamePause hook  */
+        if (!Hooks.events.renderGamePause?.length) {
+            context.spin = options.spin ?? false;
+            context.icon = options.icon ?? 'systems/daggerheart/assets/logos/compatible_with_DH_logos-10.png';
+        }
 
         return context;
     }
