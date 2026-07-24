84423cf8a07d52645ef14baf312e4727877e418e
WBHarry <williambjrklund@gmail.com>
2026-07-24 10:22:52 +0200

    Added comments

--- module/dice/dhRoll.mjs
+++ module/dice/dhRoll.mjs
@@ -248,6 +248,13 @@ export default class DHRoll extends BaseRoll {
         });
     }
 
+    /**
+     * Sums the values of all instances of ActiveEffect.change values from the toggleable bonusEffects of the roll 
+     * that match a given change.key partial path. Only for use on ActiveEffects.change with strictly numerical values.
+     * @param {string} path The full or partial effect.change key 
+     * @param {string} label The label to give the modifiers
+     * @returns {[label: string, value: any]}
+     */
     getTotalBonus(path) {
         return Object.values(this.options.bonusEffects).reduce((acc, effect) => {
             if (!effect.selected) return acc;
@@ -264,6 +271,13 @@ export default class DHRoll extends BaseRoll {
         }, 0);
     }
 
+    /**
+     * Grabs all instances of ActiveEffect.change values from the toggleable bonusEffects of the roll 
+     * that match a given change.key partial path.
+     * @param {string} path The full or partial effect.change key 
+     * @param {string} label The label to give the modifiers
+     * @returns {[label: string, value: any]}
+     */
     getBonus(path, label) {
         const modifiers = [];
         for (const effect of Object.values(this.options.bonusEffects)) {
