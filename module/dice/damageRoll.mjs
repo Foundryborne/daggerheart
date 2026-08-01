7ec4afe04e3a56c15ac09d31fcc54b44a2f9f419
WBHarry <williambjrklund@gmail.com>
2026-08-01 10:01:30 +0200

    Fixed auto deal damage

--- module/dice/damageRoll.mjs
+++ module/dice/damageRoll.mjs
@@ -42,7 +42,8 @@ export default class DamageRoll extends DHRoll {
             return roll.roll;
         }
 
-        if (!config.damage) config.damage = new ChatDamageData();
+        const noExistingDamageConfig = !config.damage;
+        if (noExistingDamageConfig) config.damage = { main: null, resources: {} };
 
         if (config.damageFormula) {
             config.damage.main = await evaluateRoll(config.damageFormula);
@@ -55,6 +56,10 @@ export default class DamageRoll extends DHRoll {
         
         for (const roll of config.resourceFormulas) {
             config.damage.resources[roll.applyTo] = await evaluateRoll(roll);
+        }
+
+        if (noExistingDamageConfig) {
+            config.damage = new ChatDamageData(config.damage);
         }
 
         roll._evaluated = true;
