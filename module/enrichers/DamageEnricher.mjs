2b1f8548c9fd877df91995847ce74d85ec5d05c0
WBHarry <williambjrklund@gmail.com>
2026-08-28 17:32:18 +0200

    Fixed so DamageEnricher can take in a resource type instead of just damage

--- module/enrichers/DamageEnricher.mjs
+++ module/enrichers/DamageEnricher.mjs
@@ -1,31 +1,35 @@
 import { parseInlineParams } from './parser.mjs';
+import { abilityCosts } from '../config/generalConfig.mjs';
 
+const { fear, resource, ...resourceTypes } = abilityCosts;
 export function DhDamageEnricher(match, _options) {
     const { value, type, inline } = parseInlineParams(match[1], { first: 'value' });
-    if (!value || !type) return match[0];
-    return getDamageMessage(value, type, inline, match[0]);
-}
-
-function getDamageMessage(damage, type, inline, defaultElement) {
-    const typeIcons = type
+    
+    const allApplicableTypes = ['physical', 'magical', ...Object.keys(resourceTypes)];
+    const types = (type ?? '')
         .replace('[', '')
         .replace(']', '')
         .split(',')
+
+    if (!value || !types.every(type => allApplicableTypes.includes(type))) return match[0];
+
+    return getDamageMessage(value, types, inline);
+}
+
+function getDamageMessage(damage, types, inline) {
+    const iconNodes = types
         .map(x => x.trim())
         .map(x => {
             return CONFIG.DH.GENERAL.damageTypes[x]?.icon ?? null;
         })
-        .filter(x => x);
-
-    if (!typeIcons.length) return defaultElement;
-
-    const iconNodes = typeIcons.map(x => `<i class="fa-solid ${x}"></i>`).join('');
+        .filter(x => x)
+        .map(x => `<i class="fa-solid ${x}"></i>`).join('');
 
     const dualityElement = document.createElement('span');
     dualityElement.innerHTML = `
         <button type="button" class="enriched-damage-button${inline ? ' inline' : ''}" 
             data-value="${damage}"
-            data-type="${type}"
+            data-type="[${types.join(',')}]"
             data-tooltip="${game.i18n.localize('DAGGERHEART.GENERAL.damage')}"
         >
             ${damage}
@@ -45,6 +49,21 @@ export const renderDamageButton = async event => {
         .split(',')
         .map(x => x.trim());
 
+    const damageTypes = types.filter(type => ['physical', 'magical'].includes(type));
+    const isDamage = Boolean(damageTypes.length);
+
+    const damageFormula = isDamage ? {
+        formula: value,
+        applyTo: CONFIG.DH.GENERAL.healingTypes.hitPoints.id,
+        damageTypes: damageTypes
+    } : null;
+
+    /* The enricher only takes 1 value, so we only use a single type even if multiples are erroneously passed in */
+    const resourceFormulas = !isDamage ? [{
+        formula: value,
+        applyTo: types[0]
+    }] : [];
+
     const config = {
         event: event,
         title: game.i18n.localize('Damage Roll'),
@@ -55,12 +74,8 @@ export const renderDamageButton = async event => {
         targets: Array.from(game.user.targets).map(t =>
             game.system.api.fields.ActionFields.TargetField.formatTarget(t)
         ),
-        damageFormula: {
-            formula: value,
-            applyTo: CONFIG.DH.GENERAL.healingTypes.hitPoints.id,
-            damageTypes: types
-        },
-        resourceFormulas: []
+        damageFormula,
+        resourceFormulas
     };
 
     CONFIG.Dice.daggerheart.DamageRoll.build(config);
