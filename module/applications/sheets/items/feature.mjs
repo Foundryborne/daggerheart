2b56957737d6ea33442d7357499b9626e404cec3
WBHarry <williambjrklund@gmail.com>
2026-08-28 11:20:45 +0200

    Fixed Feature sheet setup for CustomResources

--- module/applications/sheets/items/feature.mjs
+++ module/applications/sheets/items/feature.mjs
@@ -1,3 +1,4 @@
+import { Resource } from '../../../data/settings/Homebrew.mjs';
 import DHBaseItemSheet from '../api/base-item.mjs';
 
 export default class FeatureSheet extends DHBaseItemSheet {
@@ -5,7 +6,9 @@ export default class FeatureSheet extends DHBaseItemSheet {
     static DEFAULT_OPTIONS = {
         classes: ['feature'],
         actions: {
-            addActorResource: FeatureSheet.#onAddActorSheet
+            addActorResource: FeatureSheet.#onAddActorResource,
+            removeResource: FeatureSheet.#onRemoveResource,
+            resetResourceImage: FeatureSheet.#onResetResourceImage
         }
     };
 
@@ -40,6 +43,13 @@ export default class FeatureSheet extends DHBaseItemSheet {
         }
     };
 
+    _attachPartListeners(partId, htmlElement, options) {
+        super._attachPartListeners(partId, htmlElement, options);
+
+        for (const element of htmlElement.querySelectorAll('.path-field input'))
+            element.addEventListener('change', this.#onToggleActorResourceIconType.bind(this));
+    }
+
     async _prepareContext(options) {
         const context = await super._prepareContext(options);
         context.featureFormChoices = CONFIG.DH.ITEM.featureForm;
@@ -54,7 +64,67 @@ export default class FeatureSheet extends DHBaseItemSheet {
         return context;
     }
 
-    static async #onAddActorSheet() {
-        this.document.update({})
+    static async #onAddActorResource() {
+        const content = new foundry.data.fields.StringField({
+            label: game.i18n.localize('DAGGERHEART.GENERAL.name'),
+            required: true
+        }).toFormGroup({}, { name: 'name', localize: true }).outerHTML;
+
+        async function callback(_, button) {
+            const name = button.form.elements.name.value;
+            if (!name) return;
+
+            const sluggedName = name.slugify();
+            await this.document.update({ [`system.actorResources.${sluggedName}`]: {  
+                label: name
+            }})
+        }
+
+        await foundry.applications.api.DialogV2.prompt({
+            content: content,
+            rejectClose: false,
+            modal: true,
+            ok: { callback: callback.bind(this) },
+            window: {
+                title: game.i18n.localize('DAGGERHEART.ITEMS.Feature.createActorResourceTitle')
+            },
+            position: { width: 400 }
+        });
+    }
+    
+    static async #onRemoveResource(_, button) {
+        const confirmed = await foundry.applications.api.DialogV2.confirm({
+            window: {
+                title: game.i18n.localize(`DAGGERHEART.ITEMS.Feature.deleteActorResourceTitle`)
+            },
+            content: game.i18n.localize('DAGGERHEART.ITEMS.Feature.deleteActorResourceText')
+        });
+
+        if (!confirmed) return;
+
+        this.document.update({ [`system.actorResources.${button.dataset.resourceKey}`]: _del})
+    }
+
+    async #onToggleActorResourceIconType(event) {
+        const element = event.target.closest('.resource-icon-container');
+        const { resourceKey, imageKey } = element.dataset;
+
+        const current = this.document.system.actorResources[resourceKey].images[imageKey].isIcon;
+        await this.document.update({ [`system.actorResources.${resourceKey}.images.${imageKey}`]: { 
+            isIcon: !current,
+            value: ''
+        }});
+    }
+
+    static async #onResetResourceImage(_, button) {
+        const element = button.closest('.resource-icon-container');
+        const { resourceKey, imageKey } = element.dataset;
+
+        await this.document.update({
+            [`system.actorResources.${resourceKey}.images.${imageKey}`]:
+                Resource.getDefaultImageData(imageKey)
+        });
+
+        this.render();
     }
 }
