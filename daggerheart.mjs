93a04ad40a701458e4c42ff7a5b6cec1abdfddf9
Carlos Fernandez <cfern1990@gmail.com>
2026-08-31 21:59:50 -0400

    Fix create dialog when type is empty

--- daggerheart.mjs
+++ daggerheart.mjs
@@ -519,7 +519,6 @@ Hooks.on('renderDialogV2', (dialog, html) => {
     if (!defaultEntity) {
         nameInput.placeholder = cls.defaultName({});
         const emptyOption = document.createElement('option');
-        emptyOption.value = defaultEntity;
         emptyOption.selected = true;
         select.required = true;
         select.prepend(emptyOption);
