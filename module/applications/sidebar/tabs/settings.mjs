2934babfbfed2ca8333abb4b7179c708e02846f7
WBHarry <williambjrklund@gmail.com>
2026-09-02 23:34:36 +0200

    Added in the system update notification icon

--- module/applications/sidebar/tabs/settings.mjs
+++ module/applications/sidebar/tabs/settings.mjs
@@ -13,9 +13,17 @@ export default class DhSettings extends foundry.applications.sidebar.tabs.Settin
 
         infoSection.querySelector('.system')?.remove();
 
+        const systemUpdate = game.user.isGM && game.data.systemUpdate.hasUpdate
+            ? _loc('SETUP.UpdateAvailable', {
+                type: _loc('PACKAGE.Type.system'),
+                channel: game.data.system.title,
+                version: game.data.systemUpdate.version
+            })
+            : null;
+
         const element = await foundry.applications.handlebars.renderTemplate(
             'systems/daggerheart/templates/sidebar/settings/info-insert.hbs',
-            { version: game.system.version }
+            { version: game.system.version, systemUpdate }
         );
         infoSection.insertAdjacentHTML('afterend', element);
     }
