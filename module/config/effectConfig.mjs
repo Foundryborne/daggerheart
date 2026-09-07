a279e11b4d158e6254977e6e4c5325b53c97ef54
WBHarry <williambjrklund@gmail.com>
2026-09-07 18:51:29 +0200

    Added DiceChange ActiveEffect change type

--- module/config/effectConfig.mjs
+++ module/config/effectConfig.mjs
@@ -6,6 +6,9 @@ export const customChangeTypes = {
     },
     standardAttack: {
         id: 'standardAttack'
+    },
+    dice: {
+        id: 'dice'
     }
 };
 
@@ -43,5 +46,20 @@ export const activeEffectDurations = {
     custom: {
         id: 'custom',
         label: 'DAGGERHEART.CONFIG.ActiveEffectDuration.custom'
+    }
+};
+
+export const diceMode = {
+    override: {
+        id: 'override',
+        label: 'Override'
+    },
+    upgrade: {
+        id: 'upgrade',
+        label: 'Upgrade'
+    },
+    downgrade: {
+        id: 'downgrade',
+        label: 'Downgrade'
     }
 };
