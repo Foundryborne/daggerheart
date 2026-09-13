62a876bc1559870afcdc798892800f1967aae41c
Carlos Fernandez <cfern1990@gmail.com>
2026-09-13 15:22:17 -0400

    Move SC to base die

--- daggerheart.mjs
+++ daggerheart.mjs
@@ -117,6 +117,7 @@ CONFIG.ux.TokenManager = new TokenManager();
 CONFIG.debug.triggers = false;
 
 // Fix on Foundry native formula replacement for DH
+// @todo: this should maybe be roll data bolt ons
 const nativeReplaceFormulaData = Roll.replaceFormulaData;
 Roll.replaceFormulaData = function (formula, data = {}, { missing, warn = false } = {}) {
     /* Inserting global data */
@@ -127,26 +128,6 @@ Roll.replaceFormulaData = function (formula, data = {}, { missing, warn = false 
 
     formula = defaultingTypes.reduce((a, c) => a.replaceAll(`@${c.term}`, data[c.term] ?? c.default), formula);
     return nativeReplaceFormulaData(formula, data, { missing, warn });
-};
-
-foundry.utils.setProperty(foundry, 'dice.terms.Die.MODIFIERS.sc', 'selfCorrecting');
-
-/**
- * Return the configured value as result if 1 is rolled
- * Example: 6d6sc6  Roll 6d6, each result of 1 will be changed into 6
- * @param {string} modifier     The matched modifier query
- */
-foundry.dice.terms.Die.prototype.selfCorrecting = function (modifier) {
-    const rgx = /(?:sc)([0-9]+)/i;
-    const match = modifier.match(rgx);
-    if (!match) return false;
-    let [target] = match.slice(1);
-    target = parseInt(target);
-    for (const r of this.results) {
-        if (r.result === 1) {
-            r.result = target;
-        }
-    }
 };
 
 Hooks.once('init', () => {
--- module/dice/die/baseDie.mjs
+++ module/dice/die/baseDie.mjs
@@ -8,7 +8,8 @@ export default class BaseDie extends foundry.dice.terms.Die {
         h: 'hope',
         f: 'fear',
         a: 'advantage',
-        d: 'disadvantage'
+        d: 'disadvantage',
+        sc: 'selfCorrecting'
     };
 
     async rerollResult(resultToReroll) {
@@ -57,6 +58,24 @@ export default class BaseDie extends foundry.dice.terms.Die {
 
     async disadvantage() {
         this.#setDualityDiePreset('disadvantage');
+    }
+
+    /**
+     * Return the configured value as result if 1 is rolled
+     * Example: 6d6sc6  Roll 6d6, each result of 1 will be changed into 6
+     * @param {string} modifier     The matched modifier query
+     */
+    async selfCorrecting(modifier) {
+        const rgx = /(?:sc)([0-9]+)/i;
+        const match = modifier.match(rgx);
+        if (!match) return false;
+        let [target] = match.slice(1);
+        target = parseInt(target);
+        for (const r of this.results) {
+            if (r.result === 1) {
+                r.result = target;
+            }
+        }
     }
 
     async #setDualityDiePreset(dualityType) {
