2e2ac2d86e30d62cf94e24ebbb1844b9e1d9c4ea
Carlos Fernandez <cfern1990@gmail.com>
2026-09-18 06:21:54 -0400

    Support changing scene darkness via canvas control slider

--- daggerheart.mjs
+++ daggerheart.mjs
@@ -381,6 +381,10 @@ Hooks.on('ready', async () => {
     ui.effectsDisplay = new CONFIG.ui.effectsDisplay();
     ui.effectsDisplay.render({ force: true });
 
+    // Create Scene Darkness slider and add to `Scenes` apps list so that it will re-render on scene update
+    ui.sceneDarknessSlider = new applications.ui.SceneDarknessSlider();
+    game.scenes.apps.push(ui.sceneDarknessSlider);
+
     if (!(ui.compendiumBrowser instanceof applications.ui.ItemBrowser))
         ui.compendiumBrowser = new applications.ui.ItemBrowser();
 
@@ -533,6 +537,31 @@ Hooks.on('canvasTearDown', canvas => {
 Hooks.on('canvasReady', canas => {
     game.system.registeredTriggers.registerSceneTriggers(canvas.scene);
 });
+
+Hooks.on('getSceneControlButtons', controls => {
+    const sceneDarknessTool = {
+        name: 'changeSceneDarknessLevel',
+        title: 'CONTROLS.ChangeSceneDarknessLevel',
+        icon: 'fa-solid fa-circle-half-stroke',
+        visible: game.user.isGM,
+        toggle: true,
+        active: false,
+        onChange: () => {
+            ui.sceneDarknessSlider.toggleVisibility();
+        }
+    }
+    
+    const lightingControls = controls.lighting;
+    const newLightingTools = {};
+    for (const [key, value] of Object.entries(lightingControls.tools)) {
+        if (key === 'day') {
+            newLightingTools[sceneDarknessTool.name] = sceneDarknessTool;
+        }
+        newLightingTools[key] = value;
+    }
+    
+    controls.lighting.tools = newLightingTools;
+})
 
 /** Make the user to select a document type, instead of having a default doc type for them to accidentally keep */
 Hooks.on('renderDialogV2', (dialog, html) => {
