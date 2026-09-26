f4f41c6a80173a0b065a041f78d5c7e9b75b7289
Carlos Fernandez <cfern1990@gmail.com>
2026-09-25 20:38:30 -0400

    Check item instead

--- module/applications/sheets-configs/action-base-config.mjs
+++ module/applications/sheets-configs/action-base-config.mjs
@@ -218,7 +218,7 @@ export default class DHActionBaseConfig extends DaggerheartSheet(ApplicationV2) 
             const allKeys = Object.keys(CONFIG.DH.GENERAL.healingTypes);
             context.allDamageTypesUsed = allKeys.every(k => k in this.action._source.damage.resources);
             context.hasBaseDamage = this.action.damage?.main?.hasOwnProperty('includeBase');
-            context.showUseDefault = this.action.actor.type !== 'adversary' && this.action.type === 'attack';
+            context.showUseDefault = Boolean(this.action.item?.system.attack) && !this.action.baseAction && this.action.type === 'attack';
         }
 
         context.costOptions = this.getCostOptions();
